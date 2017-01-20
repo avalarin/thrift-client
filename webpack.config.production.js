9cba6a79bc5eaa215955ff7e244ef25ced12a5dc
Alexander Prokopyev <avalarin@gmail.com>
2017-01-20 15:27:42 +0300

    add configs for build

---
+++ webpack.config.production.js
@@ -0,0 +1,69 @@
+import path from 'path';
+import webpack from 'webpack';
+import validate from 'webpack-validator';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import merge from 'webpack-merge';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import BabiliPlugin from 'babili-webpack-plugin';
+import baseConfig from './webpack.config';
+
+export default validate(merge(baseConfig, {
+  devtool: 'cheap-module-source-map',
+
+  entry: [
+    'babel-polyfill',
+    './app/index'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'app/dist'),
+    publicPath: '../dist/'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.global\.css$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader'
+        )
+      },
+      {
+        test: /^((?!\.global).)*\.css$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+        )
+      },
+
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    // new BabiliPlugin(),
+    new ExtractTextPlugin('style.css', { allChunks: true }),
+    new HtmlWebpackPlugin({
+      filename: '../app.html',
+      template: 'app/app.html',
+      inject: false
+    })
+  ],
+
+  // https://github.com/chentsulin/webpack-target-electron-renderer#how-this-module-works
+  target: 'electron-renderer'
+}));
